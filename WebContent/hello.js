b57ef095c96e300720e8f88247284352a03106f6
S401-09 <S401-09@S401-09>
2016-08-17 15:50:42 +0900

    2016.08.17

---
+++ WebContent/hello.js
@@ -0,0 +1,8 @@
+// alert("Hello World");
+// window.location.href="http://www.sungkyul.ac.kr";
+document.write("<h1>Hello World</h1>");
+
+console.log("Hello World");
+console.error("Hello World");
+console.info("Hello World");
+console.warn("Hello World");
